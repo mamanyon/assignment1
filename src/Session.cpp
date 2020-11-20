ea662d6935a991adaf46c210c63b12589c89072b
bardabun <51476543+bardabun@users.noreply.github.com>
2020-11-20 22:39:37 +0200

    Update Session.cpp

--- src/Session.cpp
+++ src/Session.cpp
@@ -7,14 +7,115 @@ using namespace std;
 using json = nlohmann::json;
 
 //rule of 5
-Session::Session(const std::string &path) : g(), treeType(Root), agents(),counterCurrCycle(0) {
+//constructor
+Session::Session(const std::string &path): g({}), treeType(), agents(),counterCurrCycle(0) {
     ifstream file("config1.json");
     json j;
     file >> j;
     g = Graph(j["graph"]);
-    //for(auto& elem: j["agents"]){}
-
     treeType = TreeType(j["tree"]);
+
+    vector<int> CarryNodes = {};
+    int CarryNode;
+    Agent* agent;
+    for(auto& elem: j["agents"]){
+        if(elem.front() == "C")
+            agent= new ContactTracer();
+        else {
+            CarryNode = elem.at(1);
+            agent = new Virus(CarryNode);
+            CarryNodes.push_back(CarryNode);
+        }
+        addAgent(*agent);
+    }
+}
+
+//empty constructor
+Session::Session(): g({}), treeType(), agents(),counterCurrCycle(0) {}
+
+//copy constructor
+Session::Session(const Session& other):g(other.g),treeType(other.treeType()),
+                                       agents(std::vector<Agent *>()),counterCurrCycle(other.counterCurrCycle),
+                                       infected(other.infected){//copy constructor
+    for (auto agent : other.agents) {
+        agents.push_back(agent->clone());
+    }
+}
+
+//copy assignment operator
+const Session &Session::operator=(const Session &other) {
+    if (this != &other) {
+        clear();
+
+        g = other.g;
+        treeType = other.treeType;
+        infected = other.infected;
+        counterCurrCycle = other.counterCurrCycle;
+
+        agents.clear();
+        for(auto& agent: other.agents){
+            agents.push_back(agent->clone());
+        }
+    }
+    return *this;
+}
+/*
+Session *session = new Session();
+for (auto *agent: agents) {
+    if (Virus *v = dynamic_cast<Virus *>(agent)) {
+        Virus v1(*v);
+        session->addAgent(v1);
+    } else if (ContactTracer *c = dynamic_cast<ContactTracer *>(agent)) {
+        ContactTracer c1(*c);
+        session->addAgent(c1);
+    }
+}
+session->setGraph(g);
+session->treeType= getTreeType();
+session->counterCurrCycle = getCurrCycle();
+session->infected = infected;
+
+return session;
+}
+*/
+
+//move constructor
+Session::Session(Session &&other): g(other.g),treeType(other.treeType),
+                                   agents(other.agents),counterCurrCycle(other.counterCurrCycle),
+                                   infected(other.infected){
+
+    other.agents.clear();
+}
+
+//move assignment operator
+const Session &Session::operator=(Session &&other): {
+if(this != &other) {
+clear();
+
+g = other.g;
+treeType = other.treeType();
+counterCurrCycle = other.counterCurrCycle;
+agents = other.agents;
+infected = other.infected;
+
+other.agents.clear();
+}
+
+return *this;
+}
+
+//destructor
+Session::~Session() {
+    clear();
+}
+
+void Session::clear(){
+    for(auto& agent: agents){
+        if(agent != nullptr) {
+            delete agent;
+            agent = nullptr;
+        }
+    }
 }
 
 TreeType Session::getTreeType() const {
@@ -27,13 +128,6 @@ void Session::addAgent(const Agent &agent) {
     agents.push_back(newAgent);
 }
 
-//copy constructor
-Session::Session(const Session &other) : g(other.getGraph()), treeType(other.getTreeType()), agents(),
-                                         counterCurrCycle(other.getCurrCycle()) {
-    for (int i = 0; i < other.agents.size(); i++) {
-
-    }
-}
 
 int Session::getCurrCycle() const {
     return counterCurrCycle;
@@ -65,14 +159,6 @@ void Session::simulate() {
     }
 }
 
-//destructor
-Session::~Session() {
-    for (auto &agent:agents)
-        if (agent != nullptr)
-            delete agent;
-    agents.clear();
-}
-
 void Session::setGraph(const Graph &graph) { g = graph; }
 
 const Graph &Session::getGraph() const {
@@ -91,4 +177,19 @@ int Session::dequeueInfected() {
     int output = infectedQueue.front();
     infectedQueue.pop();
     return output;
+}
+
+void Session::createOutput() {
+    json outputJson;
+
+    std::vector<int> *infected;
+    for (int i = 0; i < g.getGraphSize(); i++) {
+        if(g.isInfected()) {
+            infected->push_back(i);
+        }
+    }
+    outputJson["graph"] = g.getEdges();
+    outputJson["infected"] = *infected;
+
+    std::ofstream outputFile("output.json");
 }
